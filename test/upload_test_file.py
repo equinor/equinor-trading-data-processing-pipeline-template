2984a84a6a313ec80e6565a18d4975d89155644f
Joachim Holwech <joachim.holwech@gmail.com>
2022-01-20 11:28:30 +0100

    Minor changes to upload_test_file script

--- test/upload_test_file.py
+++ test/upload_test_file.py
@@ -3,8 +3,8 @@ from azureml.core import Workspace, Datastore
 
 ws = Workspace.from_config()
 
-# %%
 ds = ws.get_default_datastore()
 
+ds.upload_files(["test.txt"], target_path="test-data", overwrite=True)
+
 # %%
-ds.upload_files(["test.txt"], overwrite=True)
